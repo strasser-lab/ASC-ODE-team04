746238b1607ac0d8a91bab0a79870751b32aa2df
Christian Strasser <e12009631@student.tuwien.ac.at>
2025-11-27 14:09:44 +0100

    merged the new exercise

---
+++ mechsystem/mass_spring.cpp
@@ -0,0 +1,33 @@
+#include "mass_spring.hpp"
+#include "Newmark.hpp"
+
+int main()
+{
+  MassSpringSystem<2> mss;
+  mss.SetGravity( {0,-9.81} );
+  auto fA = mss.AddFix( { { 0.0, 0.0 } } );
+  auto mA = mss.AddMass( { 1, { 1.0, 0.0 } } );
+  mss.AddSpring ( { 1, 10, { fA, mA } }  );
+
+  auto mB = mss.AddMass( { 1, { 2.0, 0.0 } } );
+  mss.AddSpring ( { 1, 20, { mA, mB } } );
+
+  std::cout << "mss: " << std::endl << mss << std::endl;
+
+
+  double tend = 10;
+  double steps = 1000;
+  
+  Vector<> x(2*mss.Masses().size());
+  Vector<> dx(2*mss.Masses().size());  
+  Vector<> ddx(2*mss.Masses().size());  
+
+  auto mss_func = std::make_shared<MSS_Function<2>> (mss);
+  auto mass = std::make_shared<IdentityFunction> (x.size());
+
+  mss.GetState (x, dx, ddx);
+  
+  SolveODE_Newmark(tend, steps, x, dx,  mss_func, mass,
+                   [](double t, VectorView<double> x) { std::cout << "t = " << t
+                                                             << ", x = " << Vec<4>(x) << std::endl; });
+}
